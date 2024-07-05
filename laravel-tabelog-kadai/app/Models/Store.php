e9ffb27b4f81b957ec7f9e537d4758d6f22dd2f1
kanenosuke-github <shun.216.nosuke@gmail.com>
2024-07-05 17:09:36 +0900

    store-index0705

--- laravel-tabelog-kadai/app/Models/Store.php
+++ laravel-tabelog-kadai/app/Models/Store.php
@@ -5,12 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Store extends Model
 {
     use HasFactory;
 
-    public function stores()
+    public function category()
     {
-        return $this->hasMany(Store::class);
+        return $this->belongsTo(Category::class);
     }
 }
