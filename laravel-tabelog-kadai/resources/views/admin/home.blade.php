0f6e530e485f89d3e2ead25bae3504654aaa5b43
kanenosuke-github <shun.216.nosuke@gmail.com>
2024-07-12 21:26:39 +0900

    home.blade.php.copy0712

--- laravel-tabelog-kadai/resources/views/admin/home.blade.php
+++ laravel-tabelog-kadai/resources/views/admin/home.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <title>admin-login</title>
-</head>
-<body>
-    <a href="{{route('admin.stores.index')}}">店舗一覧</a>
+@extends('layouts.app')
+
+@section('content')
     <div class="col container">
         <div class="row justify-content-center">
             <div class="col-xxl-9 col-xl-10 col-lg-11">
@@ -64,5 +57,4 @@
             </div>
         </div>
     </div>
-    </body>
-    </html>
+@endsection
